c1182c79c95b3e9046d5589ff888b643a29556eb
Jeremy Robert Musser <jemusser@indiana.edu>
2015-02-22 07:46:16 -0500

    minor settings change

--- unisencoder/settings.py
+++ unisencoder/settings.py
@@ -12,7 +12,7 @@ UNISENCODER_ROOT = os.path.dirname(os.path.abspath(__file__)) + os.sep + os.pard
 SCHEMA_DIR = UNISENCODER_ROOT + 'schema' + os.sep
 RSPEC3_SCHEMA_DIR = SCHEMA_DIR + 'rspec' + os.sep + '3' + os.sep
 
-EXNODE_LIFETIME = 10 # extent lifetime in HOURS
+EXNODE_LIFETIME = 720 # extent lifetime in HOURS
 
 XND_FILE_PATH = "/home/jemusser/exnodes" 
 DISPATCH_LOG_PATH = os.path.dirname(os.path.abspath(__file__)) + os.sep + 'dispatched_files.log'
