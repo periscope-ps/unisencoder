e8b4f8a42df6c7137cf129d86519b3eae6ac1b86
Jeremy Robert Musser <jemusser@indiana.edu>
2015-02-25 10:28:19 -0500

    Added rootname to settings

--- unisencoder/settings.py
+++ unisencoder/settings.py
@@ -13,6 +13,7 @@ SCHEMA_DIR = UNISENCODER_ROOT + 'schema' + os.sep
 RSPEC3_SCHEMA_DIR = SCHEMA_DIR + 'rspec' + os.sep + '3' + os.sep
 
 EXNODE_LIFETIME = 720 # extent lifetime in HOURS
+ROOT_NAME = "Lansat"
 
 XND_FILE_PATH = "/home/jemusser/exnodes" 
 DISPATCH_LOG_PATH = os.path.dirname(os.path.abspath(__file__)) + os.sep + 'dispatched_files.log'
